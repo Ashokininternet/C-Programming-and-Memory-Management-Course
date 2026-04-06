9f09518655263bc0e902361aac47e1f06930926a
Ashok <ashok.106sr@gmail.com>
2026-04-06 20:42:04 +0530

    Learn variable and printf and scanf in c

--- variables.c
+++ variables.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello world \n");
+    char name[81];
+    int age = 10;
+    printf("Pls enter your Name: ");
+    scanf("%s", name);
+    printf("Hello i am %s \n", name);
+    age = 20;
+    printf("My age is %d \n", age);
+
 }
