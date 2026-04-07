10a0ff64ced8b3646026f5db9ea6eb63a22c0451
Ashok <ashok.106sr@gmail.com>
2026-04-07 15:31:02 +0530

    Learn data types

---
+++ DataTypes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    double num = 12.02;
+    printf("Output = %.2f \n", num);
+
+    return 0;
+}
--- variables.c
+++ variables.c
@@ -10,4 +10,5 @@ int main() {
     age = 20;
     printf("My age is %d \n", age);
 
+    return 0;
 }
