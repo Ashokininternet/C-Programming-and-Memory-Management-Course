32da4531bbcaeb43ae2300bc1ea52e3c91dcd301
Ashok <ashok.106sr@gmail.com>
2026-04-07 23:33:27 +0530

    learn abt constants

---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //const varuable should be caps
+    const int CONST_NUM = 5;
+    // num = 4;
+    printf("%d", CONST_NUM);
+
+    return 0;
+}
