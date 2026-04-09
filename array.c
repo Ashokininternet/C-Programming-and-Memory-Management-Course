195ae5f200d9bc04cd88d3d555f0e87db3515cc2
Ashok <ashok.106sr@gmail.com>
2026-04-09 19:03:42 +0530

    learn how to use arrays and char

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // int array_nums[20] = {4, 54, 8, 8};
+    // array_nums[3] = 32;
+    // printf("%d", array_nums[3]);
+    //also an array
+    //char name[6] = "Ashok";
+
+    int array_nums[20];
+    array_nums[3] = 32;
+    array_nums[0] = 2;
+    printf("%d", array_nums[0]);
+
+
+    return 0;
+}
