5b93d60d9ce719b45bb29576607c308afa636727
Ashok <ashok.106sr@gmail.com>
2026-04-12 00:10:10 +0530

    Learn for loops in C

---
+++ for-loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // int j;
+    // int i;
+    // for (j = 1; j <=5; j++)
+    // {
+    //     printf("%d \n", j);
+
+    //     for (i = 1; i <= j; i++)
+    //     {
+    //         printf("%d", i);
+    //     }
+    // }
+
+    int luckyNum[] = {1, 2, 4, 5, 8, 9, 45, 47};
+    
+    int i;
+    for (i = 0; i <= luckyNum[i]; i++)
+    {
+        printf("%d \n", luckyNum[i]);
+    }
+    
+
+    return 0;
+}
