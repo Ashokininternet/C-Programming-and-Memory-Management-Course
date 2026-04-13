22dbbdc4a2b36e4bd5b81f78aaf6854275de3503
Ashok <ashok.106sr@gmail.com>
2026-04-13 19:16:36 +0530

    Learn typedef

---
+++ typedef.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Num;
+typedef char String[6];
+
+int main() {
+    Num x = 546;
+    printf("%d \n", x);
+
+    String name = "Ashok";
+    printf("%s \n", name);
+    return 0;
+}
