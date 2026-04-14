8323c84a0fe7ed63b2faaf314d7e7cd1ad5e822e
Ashok <ashok.106sr@gmail.com>
2026-04-15 01:13:21 +0530

    Learn more abt pointers

--- pointers.c
+++ pointers.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void fun (int* pZ){
+  (*pZ)++;
+}
 
 int main() {
     int z = 4;
     int *pZ = &z;
     printf("z = %p \n", &z);
     printf("pZ = %p \n", pZ);
-
+    fun(pZ);
+    printf("%d", z);
     return 0;
 }
