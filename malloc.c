ab2e8ae61954fd47d69763d74e67c330844f6c90
Ashok S.R <ashok.106sr@gmail.com>
2026-04-18 01:08:27 +0530

    Learn malloc

---
+++ malloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int num = 0;
+  printf("Enter the value :");
+  scanf("%d", &num);
+  char *array = malloc(num * sizeof(char));
+
+  if(array == NULL){
+    printf("Memmory not found");
+    return 1;
+  }
+  
+  for (int i = 0; i < num; i++) {
+    printf("Enter value NO: %d: ", i + 1);
+    scanf(" %c", &array[i]);
+  }
+
+  for(int i = 0; i < num; i++){
+   printf("%c ", array[i]);
+  }
+
+  free(array);
+  array = NULL;
+  return 0;
+}
