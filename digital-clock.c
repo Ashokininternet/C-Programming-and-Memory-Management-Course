4d009f94200c4a64d6bff319a4cb5fd6eb9037de
Ashok S.R <ashok.106sr@gmail.com>
2026-07-02 10:18:36 +0530

    fix the digital-clock to work in linux

--- digital-clock.c
+++ digital-clock.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-int main()
-{
- time_t rawtime = 0;
- struct tm *pTime = NULL;
- bool isRunning = true;
+int main() {
+  time_t rawtime = 0;
+  struct tm *pTime = NULL;
+  bool isRunning = true;
 
+  while (isRunning) {
+    time(&rawtime);
+    pTime = localtime(&rawtime);
 
- while (isRunning) {
-  time( &rawtime);
-  pTime = localtime(&rawtime);
+    system("clear");
 
-   system("cls");
-
-   printf( "Clock \n");
-   printf("Date:%02d:%02d:%02d\n",pTime->tm_mday, pTime->tm_mon + 1, pTime->tm_year + 1900);
-   printf("Time:%02d:%02d:%02d\n", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
-   fflush(stdout);
-   sleep(1);
- }
+    printf("Clock \n");
+    printf("Date:%02d:%02d:%02d\n", pTime->tm_mday, pTime->tm_mon + 1,
+           pTime->tm_year + 1900);
+    printf("Time:%02d:%02d:%02d\n", pTime->tm_hour, pTime->tm_min,
+           pTime->tm_sec);
+    fflush(stdout);
+    sleep(1);
+  }
   return 0;
 }
